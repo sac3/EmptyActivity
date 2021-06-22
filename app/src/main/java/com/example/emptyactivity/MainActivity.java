808d2087933f5f80b20bce73f9564575df6ef899
sac3 <sachinsyam@gmail.com>
2021-06-22 17:37:17 +0530

    android activities

--- app/src/main/java/com/example/emptyactivity/MainActivity.java
+++ app/src/main/java/com/example/emptyactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.emptyactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Life cycle event","In onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Life cycle event","In onStart");
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Life cycle event","In onPause");
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("Life cycle event","In onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Life cycle event","In onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Life cycle event","In onDestroy");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Life cycle event","In onRestart");
+
     }
 }
